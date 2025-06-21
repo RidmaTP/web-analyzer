055cfca53a37390fdaa1e8f79f17db3629c9e7da
Axalate <ridmatharaka99@gmail.com>
2025-06-21 23:43:44 +0530

    feat: result model definition

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,1 +1,17 @@
 package models
+
+type Result struct {
+	Version       string
+	Title         string
+	Headers       map[string]int
+	InternalLinks LinksData
+	ExternalLinks LinksData
+	ActiveLinks   LinksData
+	InactiveLinks LinksData
+	IsLogin       bool
+}
+
+type LinksData struct {
+	Count int
+	Links []string
+}
